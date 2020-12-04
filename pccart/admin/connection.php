dbcb14865e16f5460a2b5934d99158565bbcf38d
Suyash Chauhan <suyashchauhan31@gmail.com>
2020-12-04 17:46:06 +0530

    dotenv implemented and example also provided

--- pccart/admin/connection.php
+++ pccart/admin/connection.php
@@ -1,8 +1,9 @@
 <?php
-include_once('dbvars.php');
-
- date_default_timezone_set("Asia/Kolkata");
- $cat_image_location = 'upload/category-image/';
- $prod_image_location = 'upload/product-image/';
- $conn = mysqli_connect($DB_HOST,$DB_USER,$DB_PASSWORD,$DB_NAME) or die('Unable to connect with database');
-?>
+$path = dirname(__DIR__,2) .  "/vendor/autoload.php";
+require($path);
+$dotenv =  Dotenv\Dotenv::createImmutable(dirname(__DIR__, 1));
+$dotenv->load();
+date_default_timezone_set("Asia/Kolkata");
+$cat_image_location = 'upload/category-image/';
+$prod_image_location = 'upload/product-image/';
+$conn = mysqli_connect($_ENV['DB_HOST'], $_ENV['DB_USER'], $_ENV['DB_PASSWORD'], $_ENV['DB_NAME']) or die('Unable to connect with database');
