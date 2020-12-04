9e53969fbb15f42639532b91b0fe4f831926af89
Suyash Chauhan <suyashchauhan31@gmail.com>
2020-12-04 18:28:45 +0530

    Connection.php changed for Different environments(prod,dev)

--- pccart/admin/connection.php
+++ pccart/admin/connection.php
@@ -1,9 +1,16 @@
 <?php
-$path = dirname(__DIR__,2) .  "/vendor/autoload.php";
-require($path);
-$dotenv =  Dotenv\Dotenv::createImmutable(dirname(__DIR__, 1));
-$dotenv->load();
+
 date_default_timezone_set("Asia/Kolkata");
 $cat_image_location = 'upload/category-image/';
 $prod_image_location = 'upload/product-image/';
-$conn = mysqli_connect($_ENV['DB_HOST'], $_ENV['DB_USER'], $_ENV['DB_PASSWORD'], $_ENV['DB_NAME']) or die('Unable to connect with database');
+// for development environment
+if ($_SERVER['HTTP_HOST'] == 'localhost') {
+    $path = dirname(__DIR__, 2) .  "/vendor/autoload.php";
+    require($path);
+    $dotenv =  Dotenv\Dotenv::createImmutable(dirname(__DIR__, 1));
+    $dotenv->load();
+    $conn = mysqli_connect($_ENV['DB_HOST'], $_ENV['DB_USER'], $_ENV['DB_PASSWORD'], $_ENV['DB_NAME']) or die('Unable to connect with database');
+} else {
+    //for production environment 
+    $conn = mysqli_connect(getenv('DB_HOST'), getenv('DB_USER'), getenv('DB_PASSWORD'), getenv('DB_NAME')) or die('Unable to connect with database');
+}
